7303d25077bb7adc6c829dc9b2d721428665da5b
JCH021 <sttevan21@gmail.com>
2026-02-18 18:58:00 +0100

    remove svelte from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel({
     runtime: 'nodejs20.x', // 
